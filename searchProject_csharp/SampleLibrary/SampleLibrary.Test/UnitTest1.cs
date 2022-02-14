f96f84dbd3de3dd8fafd4cf669ec034cd530e8c4
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-14 14:13:36 +0330

    writing tests for hashSetOperatorsClass

--- searchProject_csharp/SampleLibrary/SampleLibrary.Test/UnitTest1.cs
+++ searchProject_csharp/SampleLibrary/SampleLibrary.Test/UnitTest1.cs
@@ -1,12 +1,16 @@
 using Xunit;
+using System.Collections.Generic;
 
 namespace SampleLibrary.Test;
 
 public class UnitTest1
 {
+
+    
     [Fact]
     public void Test1()
     {
         
+
     }
 }
