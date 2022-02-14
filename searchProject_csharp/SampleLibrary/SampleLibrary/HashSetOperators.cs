15c60ec1a73b68fadd7dec36015b1be46d7e71c2
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-14 16:06:52 +0330

    hashSetOperators implementation

--- searchProject_csharp/SampleLibrary/SampleLibrary/HashSetOperators.cs
+++ searchProject_csharp/SampleLibrary/SampleLibrary/HashSetOperators.cs
@@ -2,18 +2,21 @@ namespace SampleLibrary;
 
 public class HashSetOperators
 {
-     public static HashSet<int> Sub(HashSet<int> hashSet1, HashSet<int> hashSet2){
-         throw new NotImplementedException();
+    public static HashSet<int> Sub(HashSet<int> hashSet1, HashSet<int> hashSet2){
+         return hashSet1.Where(set => !hashSet2.Contains(set)).ToHashSet();
     }
 
-
     public static HashSet<int> And (List<HashSet<int>> hashSets){
-        throw new NotImplementedException();
+
+        if (hashSets.Count == 0){
+            return new HashSet<int>();
+        }
+        return hashSets[0].Where
+            (element => hashSets.TrueForAll
+                (set => set.Contains(element))).ToHashSet();
     }
 
     public static HashSet<int> Or (List<HashSet<int>> hashSets){
-        throw new NotImplementedException();
+        return hashSets.SelectMany(set => set).ToHashSet();
     }
-
-
 }
