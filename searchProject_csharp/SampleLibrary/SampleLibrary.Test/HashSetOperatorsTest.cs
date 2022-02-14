8dfa1a64190c358319626fbefca1117309f5671b
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-14 13:22:31 +0330

    create project

---
+++ searchProject_csharp/SampleLibrary/SampleLibrary.Test/HashSetOperatorsTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using SampleLibrary;
+using FluentAssertions;
+
+namespace SampleLibrary.Test
+{
+    public class HashSetOperatorsTest
+    {
+        [Fact]
+        public void hoho(){
+            Assert.Equal(1,1);
+        }
+
+
+    }
+}
