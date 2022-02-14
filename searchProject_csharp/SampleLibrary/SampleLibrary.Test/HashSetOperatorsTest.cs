3fa00550b4a5f700b1694575a0812537c1fadf69
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-14 11:39:18 +0330

    creating search project

---
+++ searchProject_csharp/SampleLibrary/SampleLibrary.Test/HashSetOperatorsTest.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace SampleLibrary.Test
+{
+    public class HashSetOperatorsTest
+    {
+        [Fact]
+        public void hoho(){
+            Assert.Equal(1,1);
+        }
+
+        
+    }
+}
