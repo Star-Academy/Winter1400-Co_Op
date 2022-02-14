ed92a86f70c5a4d94c9fe339fe550a9c51e5bec2
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-14 16:26:13 +0330

    writing test of queries class and its implimentation

---
+++ searchProject_csharp/SampleLibrary/SampleLibrary.Test/QueriesTest.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using System.Collections.Generic;
+using SampleLibrary;
+namespace SampleLibrary.Test;
+
+public class QueriesTest
+{
+    
+    [Fact]
+    public void test(){
+        Queries queries = new Queries("one two three +four +five -six -seven -eight -nine");
+        var expectedZeroQueries = new List<string>();
+        expectedZeroQueries.Add("one");
+        expectedZeroQueries.Add("two");
+        expectedZeroQueries.Add("three");
+
+        var expectedPlusQueries = new List<string>();
+        expectedPlusQueries.Add("four");
+        expectedPlusQueries.Add("five");
+
+        var expectedMinusQueries = new List<string>();
+        expectedMinusQueries.Add("six");
+        expectedMinusQueries.Add("seven");
+        expectedMinusQueries.Add("eight");
+        expectedMinusQueries.Add("nine");
+
+        Assert.Equal(queries.zeroQueries, expectedZeroQueries);
+        Assert.Equal(queries.plusQueries, expectedPlusQueries);
+        Assert.Equal(queries.minusQueries, expectedMinusQueries);
+    }
+}
