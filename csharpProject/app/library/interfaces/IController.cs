1965a9746e37269e40846d81f38be12be80aa511
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-02-16 13:20:23 +0330

    git ignore

---
+++ csharpProject/app/library/interfaces/IController.cs
@@ -0,0 +1,6 @@
+namespace library.interfaces;
+
+public interface IController
+{
+        public void Run(string studentsPath, string scoresPath, IOutput output);
+}
---
+++ csharpProject/app/library/interfaces/IOutput.cs
@@ -0,0 +1,6 @@
+namespace library.interfaces;
+
+public interface IOutput
+{
+        public void OutputList<T>(List<T> list);
+}
